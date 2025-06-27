97be0dabde945131085429f5b34ec4fd50784561
Виктор Лобынцев <sibfight@mail.ru>
2025-06-27 16:34:17 +0300

    for при вводе невалидных значений + выход по q

--- 2-calc/main.go
+++ 2-calc/main.go
@@ -31,39 +31,68 @@ func main() {
 func setOperations() (string, error) {
 	var input string
 
-	fmt.Print("Введите предполагаемую операцию (AVG - среднее, SUM - сумма, MED - медиана): ")
-	_, err := fmt.Scan(&input)
+	for {
+		fmt.Print("Введите предполагаемую операцию (AVG - среднее, SUM - сумма, MED - медиана, Q - выход): ")
+		_, err := fmt.Scan(&input)
 
-	if err != nil {
-		return "", err
+		if err != nil {
+			fmt.Println("Ошибка ввода: ", err)
+			continue
+		}
+
+		input = strings.ToUpper(strings.TrimSpace(input))
+
+		if input == "Q" {
+			return "", fmt.Errorf("выход по запросу пользователя")
+		}
+
+		if input == "AVG" || input == "SUM" || input == "MED" {
+			return input, nil
+		}
+
+		fmt.Println("Недопустимая операция. Повторите ввод.")
 	}
-
-	return strings.ToUpper(input), nil
 }
 
 func setNumbers() ([]float64, error) {
-	var input string
-	var numbers []float64
+	for {
+		fmt.Print("Введите числа через (1, 1.2, 3) или 'Q' для выхода: ")
+		var input string
+		_, err := fmt.Scan(&input)
 
-	fmt.Print("Введите числа через (,): ")
-	_, err := fmt.Scan(&input)
-
-	if err != nil {
-		return numbers, err
-	}
-
-	input = strings.TrimSpace(input)
-
-	for _, v := range strings.Split(input, ",") {
-		i, err := strconv.ParseFloat(v, 64)
 		if err != nil {
-			return numbers, err
+			fmt.Println("Ошибка чтения строки:", err)
+			continue
 		}
 
-		numbers = append(numbers, i)
-	}
+		input = strings.TrimSpace(input)
 
-	return numbers, nil
+		if strings.ToUpper(input) == "Q" {
+			return nil, fmt.Errorf("выход по запросу пользователя")
+		}
+
+		parts := strings.Split(input, ",")
+
+		var numbers []float64
+		valid := true
+
+		for _, part := range parts {
+			numStr := strings.TrimSpace(part)
+			num, err := strconv.ParseFloat(numStr, 64)
+			if err != nil {
+				fmt.Printf("Ошибка: '%s' не является числом.\n", numStr)
+				valid = false
+				break
+			}
+			numbers = append(numbers, num)
+		}
+
+		if valid && len(numbers) > 0 {
+			return numbers, nil
+		}
+
+		fmt.Println("Ввод невалиден. Попробуйте снова.")
+	}
 }
 
 func calculate(operation string, nums []float64) (float64, error) {
@@ -79,7 +108,7 @@ func calculate(operation string, nums []float64) (float64, error) {
 	case "MED":
 		return calculateMed(nums)
 	default:
-		return 0, fmt.Errorf("unknown operation: %s", operation)
+		return 0, fmt.Errorf("неизвестная операция: %s", operation)
 	}
 }
 
@@ -102,7 +131,8 @@ func calculateAVG(nums []float64) (float64, error) {
 }
 
 func calculateMed(nums []float64) (float64, error) {
-	newNums := nums
+	newNums := make([]float64, len(nums))
+	copy(newNums, nums)
 
 	sort.Float64s(newNums)
 
